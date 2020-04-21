f7c6d583e9f3050fe0b957fe1d2b7fcf10918c13
Lee <minitip@gmail.com>
2020-04-21 20:02:58 +0800

    change shell

--- lhci.sh
+++ lhci.sh
@@ -6,5 +6,4 @@ export LHCI_BUILD_CONTEXT__CURRENT_BRANCH=master
 export LHCI_BUILD_CONTEXT__COMMIT_MESSAGE="Periodic run "
 export LHCI_BUILD_CONTEXT__AUTHOR="No one <noreply@memezilla.com>"
 export LHCI_BUILD_CONTEXT__AVATAR_URL="https://picsum.photos/id/870/200/200"
-git commit --allow-empty -m 'Lighthouse CI Periodic run' && git push
 lhci autorun --upload.token="$LHCI_UPLOAD__TOKEN" # run lighthouse CI
