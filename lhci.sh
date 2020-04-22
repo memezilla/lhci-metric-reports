0d08286ba1e85373b84ec195b3ecd8b45b405abb
Lee <minitip@gmail.com>
2020-04-22 09:34:02 +0800

    change script

--- lhci.sh
+++ lhci.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 export LHCI_BUILD_CONTEXT__CURRENT_HASH=$(node -e "new Date().getTime().toString(16).split('').reverse().join('')")
-export LHCI_BUILD_CONTEXT__COMMIT_TIME=$(date --iso-8601=seconds)
+export LHCI_BUILD_CONTEXT__COMMIT_TIME=$(date +%Y-%m-%dT%H:%M:%S%z)
 export LHCI_BUILD_CONTEXT__CURRENT_BRANCH=master
-export LHCI_BUILD_CONTEXT__COMMIT_MESSAGE="Periodic run "
+export LHCI_BUILD_CONTEXT__COMMIT_MESSAGE="Periodic run"
 export LHCI_BUILD_CONTEXT__AUTHOR="No one <noreply@memezilla.com>"
 export LHCI_BUILD_CONTEXT__AVATAR_URL="https://picsum.photos/id/870/200/200"
-lhci autorun --upload.token="$LHCI_UPLOAD__TOKEN" # run lighthouse CI
+
