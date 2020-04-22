5da566f42c1d71273acc6519389512df970577b9
Lee <minitip@gmail.com>
2020-04-22 10:43:17 +0800

    change shell

--- lhci.sh
+++ lhci.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-export LHCI_BUILD_CONTEXT__CURRENT_HASH=$(node -e "new Date().getTime().toString(16).split('').reverse().join('')")
+# https://explainshell.com/explain?cmd=set+-euxo%20pipefail
+set -euxo pipefail
+export LHCI_BUILD_CONTEXT__CURRENT_HASH=$(head -n 4096 /dev/urandom | openssl sha1)
 export LHCI_BUILD_CONTEXT__COMMIT_TIME=$(date +%Y-%m-%dT%H:%M:%S%z)
 export LHCI_BUILD_CONTEXT__CURRENT_BRANCH=master
 export LHCI_BUILD_CONTEXT__COMMIT_MESSAGE="Periodic run"
 export LHCI_BUILD_CONTEXT__AUTHOR="No one <noreply@memezilla.com>"
 export LHCI_BUILD_CONTEXT__AVATAR_URL="https://picsum.photos/id/870/200/200"
-
+#lhci autorun
