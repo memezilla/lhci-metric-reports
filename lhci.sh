c324bad54d359bee0d4e323a92322239b7f29352
Lee <minitip@gmail.com>
2020-04-22 10:50:25 +0800

    move build context to travis.yml

--- lhci.sh
+++ lhci.sh
@@ -8,4 +8,4 @@ export LHCI_BUILD_CONTEXT__CURRENT_BRANCH=master
 export LHCI_BUILD_CONTEXT__COMMIT_MESSAGE="Periodic run"
 export LHCI_BUILD_CONTEXT__AUTHOR="No one <noreply@memezilla.com>"
 export LHCI_BUILD_CONTEXT__AVATAR_URL="https://picsum.photos/id/870/200/200"
-#lhci autorun
+lhci autorun
