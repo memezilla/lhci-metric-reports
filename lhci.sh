9c5e9f0408696cc544bf263ea742def7583e12d2
Lee <minitip@gmail.com>
2020-04-23 09:35:33 +0800

    change shell by adding npm install

--- lhci.sh
+++ lhci.sh
@@ -8,4 +8,7 @@ export LHCI_BUILD_CONTEXT__CURRENT_BRANCH=master
 export LHCI_BUILD_CONTEXT__COMMIT_MESSAGE="Periodic run"
 export LHCI_BUILD_CONTEXT__AUTHOR="No one <noreply@memezilla.com>"
 export LHCI_BUILD_CONTEXT__AVATAR_URL="https://picsum.photos/id/870/200/200"
+npm install -g @lhci/cli@0.3.x
+npm install -g lighthouse
+npm install -g lighthouse-plugin-field-performance
 lhci autorun
